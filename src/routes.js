9f576c39b0765381cc0ccb0efe533837284f7a99
Fragoso <jonas.fragoso@sap.com>
2020-02-15 21:04:44 -0300

    Allowing User to have an Avatar(file)

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import checkAuthorization from './app/middlewares/auth';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.put('/users', checkAuthorization, UserController.update);
 routes.post('/sessions', SessionController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
